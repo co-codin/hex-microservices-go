7c5d67684965883ecdcafc56e0ccdc715be70503
elijah <cuiyeqing960904@gmail.com>
2025-11-18 21:15:28 +0300

    serializers built

--- serializer/json/serializer.go
+++ serializer/json/serializer.go
@@ -1,2 +1,29 @@
 package json
 
+import (
+	"encoding/json"
+	"hex-microservice/shortener"
+
+	"github.com/pkg/errors"
+)
+
+type Redirect struct {
+
+}
+
+func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Decode")
+	}
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	output, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Encode")
+	}
+	return output, nil
+}
